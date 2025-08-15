b273d5690f05c879a42de367693447b92c38c8ad
Lorena Gonçalves <lorena.b.goncalves@aluno.senai.br>
2025-08-15 15:01:16 -0300

    fiz o digite seu nome e o botão de nom dia

--- 01_Hello_World/hello_world.py
+++ 01_Hello_World/hello_world.py
@@ -1,5 +1,9 @@
 import tkinter as tk
 
+def mostrar_nome():
+    escrever_nome = colocar_nome.get()
+    label_resultado.config(text = f"Bom dia, {escrever_nome}!")
+
 # criando uma janela
 # Tk (com a primeira maiuscula) é uma classe
 janela = tk.Tk()
@@ -18,13 +22,35 @@ label_titulo = tk.Label(janela,
                         text="Hello World!",
                         bg="#66091b",
                         font="Arial",
-                        foreground="#fefeff")
+                        foreground="#fefeff",
+                        )
 #pedir pra inserir o label na janela
 label_titulo.pack()
 
+# caixa de texto para a pessoa digitar seu nome
+colocar_nome = tk.Entry(janela)
+colocar_nome.pack()
+
+# botão para o programa desejar bom dia
+botao_nome = tk.Button(janela,
+                       text = "Desejar Bom dia",
+                       command = mostrar_nome)
+botao_nome.pack(pady=5)
+
+label_resultado = tk.Label(janela,
+                           text= "")
+label_resultado.pack()
+
+
 # permitir que a entrada e captura de texto
 label_nome = tk.Label(janela,
-                      text = "Digite seu nome: ")
+                      text = "Digite seu nome: ",
+                      bg= "#66091b",
+                      font="Arial",
+                      foreground= "#fbfbfb",
+                      height= 1)
+label_nome.pack()
+
 
 # loop pra manter a janela aberta
 janela.mainloop()
