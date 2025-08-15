969e37e09d811c91db177cbc9e5b46972e84645e
Lorena Gonçalves <lorena.b.goncalves@aluno.senai.br>
2025-08-15 15:33:42 -0300

    agora ta certo

--- 01_Hello_World/hello_world.py
+++ 01_Hello_World/hello_world.py
@@ -16,6 +16,8 @@ janela.iconbitmap("01_Hello_World/fox.ico") # caminho de um ícone
 janela.geometry("800x400+200+300") # arrumar o tamanho da tela
 janela.resizable(True, True) # pro usuário não editar o tamanho da janela
 
+
+
 # criando os widgets (os componentes)
 # criando o titulo e formatando
 label_titulo = tk.Label(janela,
@@ -26,7 +28,7 @@ label_titulo = tk.Label(janela,
                         )
 #pedir pra inserir o label na janela
 label_titulo.pack()
-# permitir que a entrada e captura de texto
+
 label_nome = tk.Label(janela,
                       text = "Digite seu nome: ",
                       bg= "#66091b",
@@ -35,7 +37,6 @@ label_nome = tk.Label(janela,
                       height= 1)
 label_nome.pack()
 
-
 # caixa de texto para a pessoa digitar seu nome
 colocar_nome = tk.Entry(janela)
 colocar_nome.pack()
@@ -43,13 +44,19 @@ colocar_nome.pack()
 # botão para o programa desejar bom dia
 botao_nome = tk.Button(janela,
                        text = "Desejar Bom dia",
-                       command = mostrar_nome)
+                       command = mostrar_nome,
+                       bg= "#b31e3b",
+                       font="Arial",
+                       foreground= "#000000")
+
 botao_nome.pack(pady=5)
 
 label_resultado = tk.Label(janela,
                            text= "")
 label_resultado.pack()
 
+
+# permitir que a entrada e captura de texto
 
 
 
