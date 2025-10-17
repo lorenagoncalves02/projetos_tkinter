5cdc74af693b60d9cc97966baed253c642c282b2
Maju-Touzo <maria.touzo@aluno.senai.br>
2025-10-17 15:01:25 -0300

    criei a tela de cadastro

--- 04_lista_tarefas/class_cadastro.py
+++ 04_lista_tarefas/class_cadastro.py
@@ -24,20 +24,65 @@ class Janela_cadastro():
                                font="arial, 15")
         self.titulo.pack(pady=(10,0))
 
+        #criando a caixa de texto do nome
+        self.entry_nome = tk.Entry(self.janela_cadastro)
+        self.entry_nome.pack()
+
         # criando a caixa de texto do usuário
-        entry_usuario = tk.Entry(self.janela_cadastro)
-        entry_usuario.pack()
+        self.entry_usuario = tk.Entry(self.janela_cadastro)
+        self.entry_usuario.pack()
 
         # criando a caixa de texto de senha
-        entry_senha = tk.Entry(self.janela_cadastro)
-        entry_senha.pack
+        self.entry_senha = tk.Entry(self.janela_cadastro)
+        self.entry_senha.pack
+
+        tk.Button(self.janela_cadastro, text="Cadastrar")
+
+        self.criar_tabela_usuario()
+
+    def criar_tabela_usuario(self):
+
+        conexao = sqlite3.connect("./bd_lista_tarefas.sqlite")
+
+        cursor = conexao.cursor()
+
+        cursor.execute("""
+                        CREATE TABLE IF NOT EXISTS usuario (
+                            nome VARCHAR(80),
+                            usuario VARCHAR(20),
+                            senha VARCHAR(20))
+                       """)
+        
+        conexao.commit()
+        conexao.close()
+
+    def inserir_usuario(self):
+        
+        conexao = sqlite3.connect("./bd_lista_tarefas.sqlite")
+
+        cursor = conexao.cursor()
+
+        nome = self.entry_nome
+        usuario = self.entry_usuario
+        senha = self.entry_senha
+
+        cursor.execute("""
+                        INSERT INTO usuario (nome, usuario, senha)
+                        VALUES (?, ?, ?)
+                       """,
+                       (nome, usuario, senha)
+                       )
+        
+        conexao.commit()
+        conexao.close
+
 
     def run(self):
         self.janela_cadastro.mainloop()
 
 # chamando sem nenhuma janela pai, so p testar
 if __name__ == "__main__":
-    janela_cadastro = Janela_cadastro()
+
+    janela_cadastro = Janela_cadastro("u65d75")
     janela_cadastro.run()
     
-        
